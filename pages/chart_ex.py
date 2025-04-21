d0f1027f2d1ff661cc3b2d26f454ac5216777eb8
naruhikohama <naruhikohama@gmail.com>
2025-04-21 18:13:58 -0300

    chore: testes de graficos e interações

--- pages/chart_ex.py
+++ pages/chart_ex.py
@@ -1,34 +1,69 @@
 import dash
-from dash import html, dcc, callback, Input, Output, State
+from dash import html, dcc, callback, Input, Output, State, no_update
 import dash_bootstrap_components as dbc
+
+import pandas as pd
+import polars as pl
+import plotly.express as px
+import logging
+
+from utils import *
 
 dash.register_page(__name__, title='Chart Example', path='/chart_ex')
 
+df_pl = pl.read_csv("assets/Portfolio_prices.csv")
+
+print(df_pl.head())
+
 layout = html.Div([
+    dcc.Store(id='store-df-data', storage_type='session'),
+    dcc.Store(id='store-date', storage_type='session'),
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(
+                id="select-ticker",
+                options=[{"label": i, "value": i} for i in df_pl['Ticker'].unique()],
+                value=["AAPL", "MS"],
+                multi=True,
+                placeholder="Select Tickers",
+                persistence=True,
+                persistence_type='session'
+            ),
+            dcc.DatePickerRange(
+                id='date-picker-range',
+                min_date_allowed=df_pl['Date'].min(),
+                max_date_allowed=df_pl['Date'].max(),
+                initial_visible_month=df_pl['Date'].max(),
+                start_date=df_pl['Date'].min(),
+                end_date=df_pl['Date'].max(),
+                persistence=True,
+                persistence_type='session'
+            ),
+        ])
+    ]),
+
+    html.Br(),
+
     dbc.Button(
         "Gráfico",
-        id="grafico1",
+        id="btn-grafico1",
         color="light",
         n_clicks=0
     ),
-    dbc.Collapse(
 
+    html.Br(),
+
+    dbc.Collapse(
         html.Div([
-            dcc.Graph(
-            id='example-graph',
-            figure={
-                'data': [{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'}],
-                'layout': {
-                    'title': 'Dash Data Visualization'
-                }
-            }
-            )
+            dcc.Graph(id='example-graph', config={'displayModeBar': False}),
         ],
         className="outro"),
         id="collapse-grafico1",
         is_open=False,
     ),
+
     html.Br(),
+
     html.Div([
         dcc.Graph(
             id='example-graph2',
@@ -43,8 +78,31 @@ layout = html.Div([
 ])
 
 @callback(
+    Output("store-df-data", "data"),
+    Input("select-ticker", "value"),
+    Input("date-picker-range", "start_date"),
+    Input("date-picker-range", "end_date"),
+)
+def filter_data(tickers, start_date, end_date):
+    logging.info(f"Tickers: {tickers}, Start Date: {start_date}, End Date: {end_date}")
+    if not tickers:
+        raise dash.exceptions.PreventUpdate
+            
+    df = (
+        df_pl
+        .filter(
+            (pl.col("Ticker").is_in(tickers)) &
+            (pl.col("Date") >= start_date) &
+            (pl.col("Date") <= end_date)
+        )
+        .to_pandas()
+    )
+
+    return df.to_dict(orient='records')
+
+@callback(
     Output("collapse-grafico1", "is_open"),
-    Input("grafico1", "n_clicks"),
+    Input("btn-grafico1", "n_clicks"),
     State("collapse-grafico1", "is_open")
 )
 def toggle_callapse_grafico1(n, is_open):
@@ -52,3 +110,24 @@ def toggle_callapse_grafico1(n, is_open):
         return not is_open
     return is_open
 
+
+@callback(
+    Output('example-graph', 'figure'),
+    Input('store-df-data', 'data')
+)
+
+def update_graph(dados):
+    if dados is None:
+        return no_update
+    dff = pd.DataFrame(dados)
+    start_date = dff['Date'].min()
+    end_date = dff['Date'].max()
+    tickers = dff['Ticker'].unique().tolist()
+
+    fig1 = finance_line(
+        dff, 
+        start_date=start_date, 
+        end_date=end_date, 
+        tickers=tickers
+    )
+    return fig1
---
+++ utils/__init__.py
@@ -0,0 +1,245 @@
+import pandas as pd
+import plotly.express as px
+
+def finance_line(df, start_date = '2020-01-01', end_date = '2025-01-01', tickers=['AAPL']):
+    dft = (
+        df
+        .loc[df['Ticker'].isin(tickers)]
+        .loc[df['Date'] >= start_date]
+        .loc[df['Date'] <= end_date]
+        )
+    fig = px.line(
+        dft, 
+        x='Date', 
+        y='Close', 
+        color='Ticker', 
+        color_discrete_sequence=[
+            '#04af43',
+            '#000000',
+            '#565656',
+            '#7ed4a6',
+        ],
+        category_orders=dict(
+            Ticker=sorted(tickers)
+        ),
+        line_dash='Ticker',
+        line_dash_sequence=[
+            'solid',
+            'dot',
+            'solid',
+            'dot',
+        ],
+        hover_data=dict(
+            Ticker=True,
+            Date=False,
+            Close=True
+        ),
+        custom_data=['Ticker', 'Date', 'Close']
+    )
+    fig.update_layout(
+        title_x=0.5, 
+        title_y=0.95, 
+        title_font=dict(size=20),
+        xaxis = dict(
+            showline=True,
+            showgrid=False,
+            showticklabels=True,
+            linecolor='rgb(204,204,204)',
+            linewidth=2,
+            ticks='outside',
+            tickcolor='rgb(204,204,204)',
+            tickfont=dict(
+                family='Montserrat, sans-serif',
+                size=12,
+                color='rgb(82,82,82)'
+            )
+        ),
+        yaxis = dict(
+            showline=False,
+            showgrid=False,
+            showticklabels=True,
+            linecolor='rgb(204,204,204)',
+            linewidth=2,
+            tickfont=dict(
+                family='Montserrat, sans-serif',
+                size=12,
+                color='rgb(82,82,82)'
+            ),
+        ),
+
+        hoverlabel=dict(
+            bgcolor='rgba(255,255,255,0.9)',
+            bordercolor='rgba(25,211,66,0.2)',
+            font_size=14,
+            font_family='Montserrat, sans-serif'
+        ),
+        legend=dict(
+            title_text='Tickers',
+            font=dict(
+                family='Montserrat, sans-serif',
+                size=12,
+                color='rgb(82,82,82)'
+            ),
+            orientation='h',
+            yanchor='top',
+            y=1.1,
+            xanchor='left',
+            x=0.01,
+            entrywidthmode='fraction',
+            entrywidth=0.15,
+        ),
+        margin=dict(
+            l=50, 
+            r=50, 
+            t=20, 
+            b=50, 
+            pad=4
+        ),
+        hovermode='x unified',
+        hoverdistance=100,
+        plot_bgcolor='rgba(255,255,255,0.95)'
+    )
+
+    fig.update_traces(
+        line=dict(width=2), 
+        marker=dict(size=4),
+
+        hovertemplate=
+            '%{customdata[0]}<br>' +
+            'US$ %{y:.2f}<extra></extra>',
+        )
+    fig.update_xaxes(
+        title_text='', 
+        title_font=dict(size=16),
+
+        showspikes=True,
+        spikecolor='rgba(0,0,0,0.5)',
+        spikethickness=1,
+        spikemode='across',
+        spikesnap='cursor',
+    ),
+    fig.update_yaxes(title_text='Price', title_font=dict(size=16))
+    # fig.show(config={'displayModeBar': False})
+
+    return fig
+
+def multiple_line_chart(df, x, y, tickers):
+    dft = (
+        df
+        .loc[df['Ticker'].isin(tickers)]
+        .loc[df['Date'] >= '2025-01-01']
+        )
+    fig = px.line(
+        dft, 
+        x=x, 
+        y=y, 
+        color='Ticker', 
+        color_discrete_sequence=[
+            '#04af43',
+            '#000000',
+            '#565656',
+            '#7ed4a6',
+        ],
+        category_orders=dict(
+            Ticker=sorted(tickers)
+        ),
+        line_dash='Ticker',
+        line_dash_sequence=[
+            'solid',
+            'dot',
+            'solid',
+            'dot',
+        ],
+        hover_data=dict(
+            Ticker=True,
+            Date=False,
+            Close=True
+        ),
+        custom_data=['Ticker', 'Date', 'Close']
+    )
+    fig.update_layout(
+        title_x=0.5, 
+        title_y=0.95, 
+        title_font=dict(size=20),
+        xaxis = dict(
+            showline=True,
+            showgrid=False,
+            showticklabels=True,
+            linecolor='rgb(204,204,204)',
+            linewidth=2,
+            ticks='outside',
+            tickcolor='rgb(204,204,204)',
+            tickfont=dict(
+                family='Montserrat, sans-serif',
+                size=12,
+                color='rgb(82,82,82)'
+            )
+        ),
+        yaxis = dict(
+            showline=False,
+            showgrid=False,
+            showticklabels=True,
+            linecolor='rgb(204,204,204)',
+            linewidth=2,
+            tickfont=dict(
+                family='Montserrat, sans-serif',
+                size=12,
+                color='rgb(82,82,82)'
+            ),
+        ),
+
+        hoverlabel=dict(
+            bgcolor='rgba(255,255,255,0.9)',
+            bordercolor='rgba(25,211,66,0.2)',
+            font_size=14,
+            font_family='Montserrat, sans-serif'
+        ),
+        legend=dict(
+            title_text='Tickers',
+            font=dict(
+                family='Montserrat, sans-serif',
+                size=12,
+                color='rgb(82,82,82)'
+            ),
+            orientation='h',
+            yanchor='top',
+            y=1.1,
+            xanchor='left',
+            x=0.01,
+            entrywidthmode='fraction',
+            entrywidth=0.15,
+        ),
+        margin=dict(
+            l=50, 
+            r=50, 
+            t=20, 
+            b=50, 
+            pad=4
+        ),
+        hovermode='x unified',
+        hoverdistance=100,
+        plot_bgcolor='rgba(255,255,255,0.95)'
+    )
+
+    fig.update_traces(
+        line=dict(width=2), 
+        marker=dict(size=4),
+
+        hovertemplate=
+            '%{customdata[0]}<br>' +
+            'US$ %{y:.2f}<extra></extra>',
+        )
+    fig.update_xaxes(
+        title_text=x, 
+        title_font=dict(size=16),
+
+        showspikes=True,
+        spikecolor='rgba(0,0,0,0.5)',
+        spikethickness=1,
+        spikemode='across',
+        spikesnap='cursor',
+    ),
+    fig.update_yaxes(title_text=y, title_font=dict(size=16))
+    # fig.show(config={'displayModeBar': False})
+
+    return fig
